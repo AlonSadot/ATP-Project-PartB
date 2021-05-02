efe37a122fb0c88168c55afb9c54eb9fec8d1277
Dizmonian <doram2013@gmail.com>
2021-05-02 08:09:51 +0300

    New update

--- src/IO/SimpleDecompressorInputStream.java
+++ src/IO/SimpleDecompressorInputStream.java
@@ -18,7 +18,7 @@ public class SimpleDecompressorInputStream extends InputStream {
     @Override
     public int read(byte[] b) throws IOException{
         byte[] list = in.readAllBytes();
-        int index=0,index2=0;
+        int index=0,index2=0,counter0=127,counter1=127,countRow=0,countCol=0;
         String x = "";
         String y = "";
         while(list[index2] != -1){
@@ -31,9 +31,34 @@ public class SimpleDecompressorInputStream extends InputStream {
             index2++;
         }
         int row = Integer.parseInt(x), column = Integer.parseInt(y);
+
         while (b[index] != -2) {
             b[index] = list[index];
             index++;
+        }
+        index++;
+
+        int num;
+
+        while (index<list.length){
+            for (countRow = 0;countRow<row;countRow++){
+                while(countCol<column){
+                    num = list[index] + 127;
+                    for (int i = 0;i<num;i++){
+                        b[index] = 0;
+                        index++;
+                        countCol++;
+                    }
+                    index++;
+                    num = list[index] + 127;
+                    for (int i = 0;i<num;i++){
+                        b[index] = 1;
+                        index++;
+                        countCol++;
+                    }
+
+                }
+            }
         }
         return 0;
     }
