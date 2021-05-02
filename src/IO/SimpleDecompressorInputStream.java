31b0a75e51f1582daf23b81b1c0df9bb90c95f38
alons <alon.sadot02@gmail.com>
2021-05-02 17:04:27 +0300

    1/5/2021

--- src/IO/SimpleDecompressorInputStream.java
+++ src/IO/SimpleDecompressorInputStream.java
@@ -1,5 +1,6 @@
 package IO;
 
+import javax.lang.model.element.NestingKind;
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -17,8 +18,31 @@ public class SimpleDecompressorInputStream extends InputStream {
 
     @Override
     public int read(byte[] b) throws IOException{
-        
+        byte[] list = in.readAllBytes();
+        int index=0;
+        while (list[index] != -2) {
+            b[index] = list[index];
+            index++;
+        }
+        b[index] = list[index];
+        index++;
+        int num , iterator = index;
+        while (index < list.length){
+            num = list[index] + 127;
+            for (int k = 0;k<num;k++){
+                b[iterator] = 0;
+                iterator++;
+            }
+            index++;
+            if(index == list.length)
+                break;
+            num = list[index] + 127;
+            for (int k = 0;k<num;k++){
+                b[iterator] = 1;
+                iterator++;
+            }
+            index++;
+        }
         return 0;
     }
-
 }
